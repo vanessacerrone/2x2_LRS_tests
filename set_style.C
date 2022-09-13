4b6eb17934909e90f55743fd2dee4b6a12fe4dc4
Vanessa Cerrone <vanessa.cerrone.1@gmail.com>
2022-09-13 14:30:38 -0500

    Update set_style.C

--- set_style.C
+++ set_style.C
@@ -16,8 +16,8 @@ void style(TStyle* gStyle) {
    	gStyle->SetPadRightMargin(0.15);
    	gStyle->SetPadTopMargin(0.08);
 	gStyle->SetPadBottomMargin(0.125);
-    gStyle->SetLegendBorderSize(0);
-    gStyle->SetLegendTextSize(0.03);
+        gStyle->SetLegendBorderSize(0);
+        gStyle->SetLegendTextSize(0.03);
 	gStyle->SetPadBorderSize(2);
 
 }
@@ -25,22 +25,22 @@ void style(TStyle* gStyle) {
 void histo_style(TH1D* h) {
 
 	h->SetTitle("");
-    // x axis
+    	// x axis
    	h->GetXaxis()->SetLabelSize(0.03);
    	h->GetXaxis()->SetTitleSize(0.03);
    	h->GetXaxis()->SetTitleOffset(1);
    	h->GetXaxis()->SetTickLength(0.02);
-    h->GetXaxis()->SetMaxDigits(3);
+    	h->GetXaxis()->SetMaxDigits(3);
 
-    // y axis
+    	// y axis
    	h->GetYaxis()->SetLabelSize(0.03);
    	h->GetYaxis()->SetTitleSize(0.03);
    	h->GetYaxis()->SetTitleOffset(1.35);   
    	h->GetYaxis()->SetTickLength(0.02); 
 	h->GetYaxis()->SetMaxDigits(3);
 
-    // fill and line
-    //h->SetFillStyle(3001); 
+    	// fill and line
+    	//h->SetFillStyle(3001); 
 	Int_t ci; // color index setting
 	ci = TColor::GetColor("#004C97");
 	h->SetFillColorAlpha(ci,0.2);
@@ -50,19 +50,19 @@ void histo_style(TH1D* h) {
 void graph_style(TGraph* g) {
 
 	g->SetTitle("");
-    // x axis
+    	// x axis
    	g->GetXaxis()->SetLabelSize(0.035);
    	g->GetXaxis()->SetTitleSize(0.035);
    	g->GetXaxis()->SetTitleOffset(1);
    	g->GetXaxis()->SetTickLength(0.02);
-    g->GetXaxis()->SetMaxDigits(3);
+   	 g->GetXaxis()->SetMaxDigits(3);
 
-    // y axis
+   	// y axis
    	g->GetYaxis()->SetLabelSize(0.035);
    	g->GetYaxis()->SetTitleSize(0.035);
    	g->GetYaxis()->SetTitleOffset(1.5);    
    	g->GetYaxis()->SetTickLength(0.02);
-    g->GetYaxis()->SetMaxDigits(3);
+   	g->GetYaxis()->SetMaxDigits(3);
 
 }
 
