107a73ef35eee5e73c01f8b49d3e779e0d3a73ac
vanessacerrone <vanessa.cerrone.1@gmail.com>
2022-09-19 09:52:46 -0500

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # run led calibration script
-# usage: .run.sh filename LRS
-# where LRS is either LCM or ACL
+# usage: .run.sh filename 
 
 root -b -l << EOF
 
 .L led_calibration.C
-all_channels("$1",64,11,2,"$2")
+all_channels("$1",64,11,2)
 .q
 EOF
